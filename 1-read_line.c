77c14cb0ecee5fa7b4000a8ff8d218ffc01b3fb5
dariomnalerio <5725@holbertonstudents.com>
2022-11-28 09:57:27 -0800

    update

--- 1-read_line.c
+++ 1-read_line.c
@@ -6,33 +6,33 @@
  * @av: argument vector
  */
 
-int main (int ac __attribute__((unused)), char **av)
+int main (int ac __attribute__((unused)), char **av __attribute__((unused)))
 {
-	unsigned long int j, len;
-	size_t *str = NULL, *ptr = NULL;
+	unsigned long int j;
+	size_t ptr = 0;
+	char *buffer;
 
-	printf("$\n");
+	printf("$ ");
 
 	while (1)	
 	{
 		
-		ptr = malloc (sizeof(size_t));
+	/*	ptr = malloc (sizeof(size_t)); 
 		if (ptr == NULL)
 		{
 			printf("test1");
 			return (-1);
-		}
-		j = getline (av, ptr, stdin);
+		} */
+		j = getline (&buffer, &ptr, stdin);
 		if (j == -1)
 		{
 			printf("Error\n");
 			return (-1);
 		}
-		if (j = 1)
-			printf("$ ");
-		else
-			printf("%ln\n", ptr);
-		free(ptr);
+
+		printf("%s", buffer);
+		printf("$ ");
+/*		free(ptr); */
 	}
 	return (0);
 }
