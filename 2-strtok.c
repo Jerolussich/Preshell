8f0952b0ba71cf900bff551c078c41ec5fc3b203
Jerolussich <jerolussich@hotmail.com>
2022-11-29 06:18:59 -0800

    task 2

--- 2-strtok.c
+++ 2-strtok.c
@@ -12,18 +12,22 @@ int main (int ac, char **av __attribute__((unused)))
 	size_t ptr = 0;
 	char *buffer, *token, **toks;
 
-	j = getline (&buffer, &ptr, stdin);
+	j = getline(&buffer, &ptr, stdin);
 	if (j == -1)
 	{
 		printf("Error\n");
 		return (-1);
 	}
 	toks = malloc(sizeof(char *) * ac);
+	if (!toks)
+		return (0);
 	token = strtok(buffer, " ");
-	while (k = 0, token, k++)
+	for (k = 0; token; k++)
 	{
 		toks[k] = strdup(token);
 		token = strtok(NULL, " ");
+		printf("%s\n", toks[k]);
 	}
+	toks[k] = token;
 	return (0);
 }
