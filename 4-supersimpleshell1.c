c593510fc4d8e860c3728d984c9adc1f56c4bebd
Jerolussich <jerolussich@hotmail.com>
2022-12-01 10:49:19 -0800

    segmentation fault shell update

--- 4-supersimpleshell1.c
+++ 4-supersimpleshell1.c
@@ -6,7 +6,7 @@
  * @av: argument vector
  */
 
-int main (int ac, char **av __attribute__((unused)))
+int main (int ac, char **av)
 {
 	unsigned long int j,k,fk;
 	size_t ptr = 0;
@@ -14,9 +14,8 @@ int main (int ac, char **av __attribute__((unused)))
 	pid_t pid;
 	int status, check1;
 	char array[10];
+	struct stat st;
 
-
-	type_prompt();
 	while (1)
 	{
 		printf("$ ");
@@ -37,41 +36,40 @@ int main (int ac, char **av __attribute__((unused)))
 		}
 		toks[k] = token;
 
-		check1 = stat(buff, &st);
-		if (check1 == 0)
+		check1 = stat(toks[0], &st);
+		if (check1 == 0)//if given full path
 		{
-
+			fk = fork();
+			if (fk == -1)
+				return (-1);
+			if (!fk)
+				execve(toks[0], toks, NULL);
+			else
+			{
+				wait(&status);
+				free(toks);
+			}
 		}
-
-		checkar = toks[0][0];
-		if ((checkar >= 65 && checkar <= 90) || (checkar >= 97 && checkar <= 122)) // ls
+		else// if full path not given
 		{
 			path = get_env(env);
 			command = attach_path(path, toks[0]);
 			if (command == NULL)
 			{
-				printf("Command not found");
+				perror("Error: ");
 				return (-1);
 			}
-			else // if command found
-			{ // primero checkear si te pasan un path a un programa, si no es encontrado concatenar, si no es encontrado command not found
-				fk = fork();
-				if (fk == -1)
-					return(-1);
-				if(!fk)
-				{
-					execve(command, toks[0], NULL);
-				}
-				else
-					wait(&status);
-
+			fk = fork();
+			if (fk == -1)
+				return (-1);
+			if (!fk)
+				execve(command, toks, NULL);
+			else
+			{
+				wait(&status);
 				free(toks);
 			}
 		}
-		if else // /usr/bin/ls
-		else // /usr/bin/lsa || lsa || #"$#
-		attach_path(path, toks[0]);
-		free(toks);
 	}
 	return (0);
 }
